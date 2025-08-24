48a7bb34181cfd03de13a10850bb7a892cc73f00
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-08-25 00:01:49 +0200

    Bump version numbers

--- Core_BetterPenetration/Constants.cs
+++ Core_BetterPenetration/Constants.cs
@@ -3,9 +3,9 @@ namespace Core_BetterPenetration
     internal class Constants
     {
 #if KK || KKS || EC
-        public const string PluginVersion = "4.5.5.3";
+        public const string PluginVersion = "4.5.5.4";
 #else
-        public const string PluginVersion = "5.0.1.3";
+        public const string PluginVersion = "5.0.1.4";
 #endif
     }
 }
