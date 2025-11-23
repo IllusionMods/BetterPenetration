d4806cf4d0e8a0d7f5ff7897c106317254d05076
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-11-23 18:03:46 +0100

    Fix broken repo links that pointed to GH

--- Core_BetterPenetration/AssemblyInfo.cs
+++ Core_BetterPenetration/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using Core_BetterPenetration;
 
-[assembly: AssemblyCompany("https://github.com/IllusionMods/BetterPenetration")]
-[assembly: AssemblyCopyright("Copyright GPL-3.0 © 2020")]
+[assembly: AssemblyCompany("https://gitgoon.dev/IllusionMods/BetterPenetration")]
+[assembly: AssemblyCopyright("Copyright GPL-3.0 ï¿½ 2020")]
 [assembly: AssemblyProduct("BetterPenetration")]
 [assembly: AssemblyTitle("Make penetration look a bit more realistic and overall improve the HScene experience")]
 [assembly: AssemblyVersion(Constants.PluginVersion)]
